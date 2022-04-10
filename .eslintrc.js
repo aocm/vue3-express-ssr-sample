6c70776e5f2e6554426bec586acfd8dda4b8fe71
aocm <aocm.dev@gmail.com>
2022-04-11 03:40:33 +0900

    chore: prettier除去してeslintのrule追加
    - 参考 https://qiita.com/diggy-mo/items/bb01bcb54237f16bb008

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,20 +7,53 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2019, // Node.js 12の場合は2019、他のバージョンのNode.jsを利用している場合は場合は適宜変更する
+    ecmaVersion: 12,
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.eslint.json']
   },
   plugins: [
     '@typescript-eslint',
+    'vue'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:vue/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',,
-    'prettier',
-    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  rules: {
+  rules: { // 0 = off, 1 = warn, 2 = error
+    // 参考: https://qiita.com/diggy-mo/items/bb01bcb54237f16bb008
+    // 不要なカッコは消す
+    'no-extra-parens': 1,
+    // 無駄なスペースは削除
+    'no-multi-spaces': 2,
+    // 不要な空白行は削除。2行開けてたらエラー
+    'no-multiple-empty-lines': [2, {'max': 1}],
+    // 関数とカッコはあけない(function hoge() {/** */})
+    'func-call-spacing': [2, 'never'],
+    // true/falseを無駄に使うな
+    'no-unneeded-ternary': 2,
+    // セミコロンは禁止
+    'semi': [2, 'never'],
+    // 文字列はシングルクオートのみ
+    'quotes': [2, 'single'],
+    // varは禁止
+    'no-var': 2,
+    // jsのインデントは２
+    'indent': [2, 2],
+    // かっこの中はスペースなし！違和感
+    'space-in-parens': [2, 'never'],
+    // コンソールは許可
+    'no-console': 0,
+    // カンマの前後にスペース入れる？
+    'comma-spacing': 2,
+    // 配列のindexには空白入れるな(hogehoge[ x ])
+    'computed-property-spacing': 2,
+    // キー
+    'key-spacing': 2,
+    // キーワードの前後には適切なスペースを
+    'keyword-spacing': 2,
+    // 行末スペース
+    'no-trailing-spaces': 2
   },
-};
+}
