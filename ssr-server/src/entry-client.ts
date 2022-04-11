afd9b5c6307e4fa2e3c453d2c4f02db4850b7917
aocm <aocm.dev@gmail.com>
2022-04-11 17:47:00 +0900

    vuexを使わないので削除

--- ssr-server/src/entry-client.ts
+++ ssr-server/src/entry-client.ts
@@ -1,12 +1,10 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import { createRouter } from './router'
-import store from './store'
 
 const app = createSSRApp(App)
 const router = createRouter()
 
-app.use(store)
 app.use(router)
 
 router.isReady().then(() => {
