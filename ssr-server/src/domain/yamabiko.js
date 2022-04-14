3b26eaa58c5d37a0504aed6f7d0c4f44979962ea
aocm <aocm.dev@gmail.com>
2022-04-14 16:44:20 +0900

    モジュールのメソッドをmock化するサンプル実装

--- ssr-server/src/domain/yamabiko.js
+++ ssr-server/src/domain/yamabiko.js
@@ -1,13 +1,26 @@
+import yamabikoRegistry from "../infra/yamabikoRepository"
+
 export default class Yamabiko {
   #_text
+  #number
 
   get message() {
     return this.#_text
   }
 
+  get number() {
+    return this.#number
+  }
   constructor(props) {
     if (typeof props !== 'string') throw new Error('文字列じゃない')
     if (!(props.length >= 1) || !(props.length <= 20)) throw new Error('1~20文字でない')
     this.#_text = props
+    try {
+      this.#number = yamabikoRegistry.saveMessage(this.#_text)
+      console.log(this.#number)
+    } catch (e) {
+      console.error("error: ", e)
+       throw new Error('予期せぬエラーが発生')
+    }
   }
 }
---
+++ ssr-server/src/infra/yamabikoRepository.js
@@ -0,0 +1,30 @@
+// DBの代わり
+let instance;
+class Registry {
+  #messages = []
+  #count = 0
+  get message() {
+    return this.#messages
+  }
+
+  count() {
+    return this.#count
+  }
+  saveMessage(message) {
+    this.#messages.push(message)
+    this.#count++
+    return this.#count
+  }
+
+  constructor() {
+    if (instance) {
+      throw new Error("You can only create one instance!");
+    }
+    instance = this;
+  }
+}
+
+const yamabikoRegistry = new Registry()
+
+export default yamabikoRegistry
+
