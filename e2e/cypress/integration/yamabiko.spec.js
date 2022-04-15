e4eb20fbf494198c2bd207647e81a1355a2042a9
aocm <aocm.dev@gmail.com>
2022-04-15 16:22:58 +0900

    cypress見直し

--- e2e/cypress/integration/yamabiko.spec.js
+++ e2e/cypress/integration/yamabiko.spec.js
@@ -2,33 +2,29 @@ describe('Yamabiko Test', () => {
   it('link and view', () => {
     cy.visit('/')
     cy.get('#toYamabikoLink').click()
-    // FizzBuzzという文言が表示されていることを確認する
     expect(cy.get('#title').contains('Yamabiko')).to.exist
   })
   it('やまびこ', () => {
     cy.visit('/yamabiko')
     cy.get('#yamabiko-input').type('test')
     cy.get('#yamabiko-button').click()
-    // FizzBuzzという文言が表示されていることを確認する
-    cy.get('#yamabiko-response').should('have.text', 'res: test')
+    cy.get('#yamabiko-response').should('have.text', ' res: test')
   })
   it('やまびこリダイレクト', () => {
     cy.visit('/yamabiko')
     cy.get('#yamabiko-form').type('test')
     cy.get('#yamabiko-form-button').click()
-    // FizzBuzzという文言が表示されていることを確認する
     cy.url().should('eq', 'http://localhost:3000/yamabiko-res?message=test')
-    cy.get('#yamabiko-response').should('have.text', 'res: test')
+    cy.get('#yamabiko-response').should('have.text', ' res: test')
   })
   
-  context.only('やまびこエラー', () => {
+  context('やまびこエラー', () => {
 
     it('21文字以上', () => {
       cy.visit('/yamabiko')
       const length21 = '012345678901234567890'
       cy.get('#yamabiko-form').type(length21)
       cy.get('#yamabiko-form-button').click()
-      // FizzBuzzという文言が表示されていることを確認する
       cy.url().should('eq', 'http://localhost:3000/error?message=1~20%E6%96%87%E5%AD%97%E3%81%A7%E3%81%AA%E3%81%84')
     })
     it('0文字', () => {
@@ -36,7 +32,6 @@ describe('Yamabiko Test', () => {
       const length0 = ''
       // cy.get('#yamabiko-form').type(length0) // 不要
       cy.get('#yamabiko-form-button').click()
-      // FizzBuzzという文言が表示されていることを確認する
       cy.url().should('eq', 'http://localhost:3000/error?message=1~20%E6%96%87%E5%AD%97%E3%81%A7%E3%81%AA%E3%81%84')
     })
   })
